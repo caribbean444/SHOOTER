97cdf32275f2358820f7523998f3832c2c6744a8
caribbean444 <misha.bogatow@yandex.ru>
2020-11-21 20:42:31 +0300

    add

--- Meteor/Meteor.gd
+++ Meteor/Meteor.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-var maxSpeed_x2: float = 15
-var minSpeed_x2: float = -15
+var maxSpeed_x: float = 15
+var minSpeed_x: float = -15
 var minSpeed_y: float = 20
 var maxSpeed_y: float = 60
 var minRotationRate: float = -30
@@ -10,19 +10,18 @@ var maxRotationRate: float = 30
 var live: int = 10
 var speed_y: float = 0
 var rotationRate: float = 0
-var speed_x1: float = 0
-var speed_x2: float = 0 
+var speed_x: float = 0
 
 
 func _ready():
 	speed_y = rand_range(minSpeed_y, maxSpeed_y)
 	rotationRate = rand_range(minRotationRate, maxRotationRate)
-	speed_x2 = rand_range(minSpeed_x2, maxSpeed_x2)
+	speed_x = rand_range(minSpeed_x, maxSpeed_x)
 func _physics_process(delta):
 	rotation_degrees += rotationRate * delta
 	
 	position.y += speed_y * delta
-	position.x += speed_x2 * delta  
+	position.x += speed_x * delta  
 
 func _on_VisibilityNotifier2D_screen_exit():
 	queue_free()
